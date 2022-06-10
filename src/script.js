88dd250ee089b029c9b72ec9c309b144f7a47fda
sojib076 <sojibds332@gmail.com>
2022-06-10 17:43:34 +0600

    threejs

--- src/script.js
+++ src/script.js
@@ -1,73 +1,143 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-console.log(OrbitControls)
- /* Base
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
+
+/**
+ * Base
  */
+// Debug
+const gui = new dat.GUI()
 
 // Canvas
-const cursor ={
-    x:0,
-    y:0,
-   
-}
 const canvas = document.querySelector('canvas.webgl')
-// cursor
-window.addEventListener('mousemove',(event)=>{
 
-    cursor.x= event.clientX / sizes.width - 0.5
-    cursor.y= event.clientY / sizes.height - 0.5
-    console.log(cursor.y)
-})
-// Sizes
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('textures/matcaps/6.png')
+
+/**
+ * Fonts
+ */
+const fontLoader = new THREE.FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        fontLoader.load(
+            '/fonts/helvetiker_regular.typeface.json',
+            (font) =>
+            {
+                // Material
+                const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+                // Text
+                const textGeometry = new THREE.TextBufferGeometry(
+                    ' HI iam Sojib Das ',
+                    {
+                        font: font,
+                        size: 0.5,
+                        height: 0.2,
+                        curveSegments: 12,
+                        bevelEnabled: true,
+                        bevelThickness: 0.03,
+                        bevelSize: 0.02,
+                        bevelOffset: 0,
+                        bevelSegments: 5
+                    }
+                )
+                textGeometry.center()
+
+                const text = new THREE.Mesh(textGeometry, material)
+                scene.add(text)
+
+                // Donuts
+                const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 32, 64)
+
+                for(let i = 0; i < 100; i++)
+                {
+                    const donut = new THREE.Mesh(donutGeometry, material)
+                    donut.position.x = (Math.random() - 0.5) * 10
+                    donut.position.y = (Math.random() - 0.5) * 10
+                    donut.position.z = (Math.random() - 0.5) * 10
+                    donut.rotation.x = Math.random() * Math.PI
+                    donut.rotation.y = Math.random() * Math.PI
+                    const scale = Math.random()
+                    donut.scale.set(scale, scale, scale)
+
+                    scene.add(donut)
+                }
+            }
+        )
+    }
+)
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-// Scene
-const scene = new THREE.Scene()
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh)
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-//camera.position.x = 2
-//camera.position.y = 2
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
 camera.position.z = 2
-camera.lookAt(mesh.position)
 scene.add(camera)
-const controls= new OrbitControls(camera,canvas)
-controls.enableDamping= true
 
-// Renderer
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-////
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-   // mesh.rotation.y = elapsedTime;
-        // camera.position.x= Math.sin(cursor.x * Math.PI*2)  *3           //cursor.x * 10
-        // camera.position.z= Math.cos(cursor.x*Math.PI * 2) *3
-        // camera.lookAt(mesh.position)
-        // camera.position.y= cursor.y * 5
-        controls.update()
-            // Render
+    // Update controls
+    controls.update()
+
+    // Render
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
